c674e2b412b9f14742d715333b46d4ae6518e6a9
Hiep Nguyen <nguyenhieptech@gmail.com>
2025-12-19 11:19:07 +0700

    feat: add spartan-ui components and todo example

--- web-spa/src/app/app.routes.ts
+++ web-spa/src/app/app.routes.ts
@@ -1,3 +1,4 @@
 import { Routes } from "@angular/router";
+import { TodoComponent } from "./todo/todo.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [{ path: "todo", component: TodoComponent }];
