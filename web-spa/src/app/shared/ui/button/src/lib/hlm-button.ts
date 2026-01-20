7a61b7d1b3e21b82aad6354b1937f456480ef437
Hiep Nguyen <nguyenhieptech@gmail.com>
2026-01-20 22:44:13 +0700

    style: hover:cursor-pointer button

--- web-spa/src/app/shared/ui/button/src/lib/hlm-button.ts
+++ web-spa/src/app/shared/ui/button/src/lib/hlm-button.ts
@@ -6,7 +6,7 @@ import { classes } from "@/shared/ui/utils";
 import { injectBrnButtonConfig } from "./hlm-button.token";
 
 export const buttonVariants = cva(
-  "inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_ng-icon]:pointer-events-none [&_ng-icon]:shrink-0 [&_ng-icon:not([class*='text-'])]:text-base",
+  "inline-flex shrink-0 items-center justify-center gap-2 rounded-md text-sm font-medium whitespace-nowrap transition-all outline-none hover:cursor-pointer focus-visible:border-ring focus-visible:ring-[3px] focus-visible:ring-ring/50 disabled:pointer-events-none disabled:opacity-50 aria-invalid:border-destructive aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 [&_ng-icon]:pointer-events-none [&_ng-icon]:shrink-0 [&_ng-icon:not([class*='text-'])]:text-base",
   {
     variants: {
       variant: {
@@ -38,7 +38,7 @@ export const buttonVariants = cva(
 export type ButtonVariants = VariantProps<typeof buttonVariants>;
 
 @Directive({
-  selector: "button[hlmBtn], a[hlmBtn]",
+  selector: "button[hlmBtn],a[hlmBtn]",
   exportAs: "hlmBtn",
   hostDirectives: [{ directive: BrnButton, inputs: ["disabled"] }],
   host: {
